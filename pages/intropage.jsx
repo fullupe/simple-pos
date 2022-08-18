1c89b4b41aee68084a0209c3e838c22c3a5bd01c
cgeorgea60 <64281418+cgeorgea60@users.noreply.github.com>
2022-08-18 22:07:54 +0100

    second commit

--- pages/intropage.jsx
+++ pages/intropage.jsx
@@ -5,7 +5,7 @@ const Intropage = ({ name }) => {
     <>
       <div className="flex justify-center items-centter min-h-screen w-full bg-blue-400">
         <div className="w-1/2 h-64 flex justify-center items-centter my-auto">
-          <div className="w-96 flex justify-between items-center">
+          <div className="w-1/2 flex justify-between items-center bg-blue-100 drop-shadow-xl px-10 rounded-xl ">
             <Button name="pos" />
             <Button name="admin" />
           </div>
@@ -15,4 +15,3 @@ const Intropage = ({ name }) => {
   );
 };
 export default Intropage;
-
